da55b11edd3f59856393a33ec131d9e1e69e7390
unknown <daniells642@hotmail.com>
2022-10-15 12:22:33 -0300

    Topo estilizado

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './estilo.css';
 
 export default function Rodape() {
     return (
--- src/componentes/Topo/index.js
+++ src/componentes/Topo/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import '../../App.css';
+import './estilo.css';
 
 export default function Topo() {
     return (
-      <header>
-        <nav>
-            <img src="../assets/logo.png" title="Gelateria" alt="logo da gelateria" />
-            <Link className="opcao-Home" to='/'>Home</Link>
-            <Link className="opcao-Sabores" to='/Sabores'>Sabores</Link>
-            <Link className="opcao-Sobre" to='/Sobre'>Sobre</Link>
-        </nav>
+      <header className="secao-topo">
+        <div className="limitar-secao">
+            <div className="topo">  
+              <img src="../assets/logo.png" title="Gelateria" alt="logo da gelateria" />
+            </div>
+            <nav className="topo-links"> 
+              <Link className="opcao-link" to='/'>Home</Link>
+              <Link className="opcao-link" to='/Sabores'>Sabores</Link>
+              <Link className="opcao-link" to='/Sobre'>Sobre</Link>
+            </nav> 
+        </div>
 
       </header>
     );
